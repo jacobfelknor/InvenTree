05054def3400787115cb4108fedf6f4e591716f0
Jacob Felknor <jacobfelknor073@gmail.com>
2026-01-09 18:04:07 +0000

    add take from and destination serializer details

--- src/backend/InvenTree/order/serializers.py
+++ src/backend/InvenTree/order/serializers.py
@@ -2160,7 +2160,9 @@ class TransferOrderSerializer(
         model = order.models.TransferOrder
         fields = AbstractOrderSerializer.order_fields([
             'take_from',
+            'take_from_detail',
             'destination',
+            'destination_detail',
             'consume',
             'complete_date',
         ])
@@ -2172,6 +2174,14 @@ class TransferOrderSerializer(
         fields = super().skip_create_fields()
 
         return [*fields, 'duplicate']
+
+    take_from_detail = enable_filter(
+        stock.serializers.LocationSerializer(source='take_from'), default_include=True
+    )
+
+    destination_detail = enable_filter(
+        stock.serializers.LocationSerializer(source='destination'), default_include=True
+    )
 
     # TODO:
     # @staticmethod
