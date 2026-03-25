41be1a895cc270217824abd6926a67b0f638e99b
Jacob Felknor <jacobfelknor073@gmail.com>
2026-03-25 16:27:14 +0000

    bump api version

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,10 +1,13 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 467
+INVENTREE_API_VERSION = 468
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 INVENTREE_API_TEXT = """
+
+v468 -> 2026-03-25 : https://github.com/inventree/InvenTree/pull/11281
+    - Add Transfer Order to InvenTree
 
 v467 -> 2026-03-20 : https://github.com/inventree/InvenTree/pull/11573
     - Fix definition for the "parent" field on the StockItemSerializer
